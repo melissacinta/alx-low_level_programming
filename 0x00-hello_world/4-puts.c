9506725ba15b813cba85a162dd2bc3069220f848
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-13 15:13:15 -0700

    fixed bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - this os the main entry point
+ * feel free to add other things
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return 0;
+	
+	return (0)
 }
