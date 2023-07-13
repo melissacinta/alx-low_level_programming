512987ac68ee8532903971f2373e01616855c65c
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-13 15:37:19 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this os the main entry point
+ * feel free to add other things
+ *
+ * Return: returns 0 asthe exit code
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
