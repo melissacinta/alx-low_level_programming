caaed60ce85560406cf801d8cf44a00cac8a5e13
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-18 08:26:51 -0700

    0n here o

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,11 @@
 /**
  * _islower - checks for lowercase character.
  *
+ * @c : integer variable
+ *
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z')
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 
 /**
  * _isalpha - checks for alphabetic character.
+ * @c : integer variable
  * 
  * Return: Always 0.
  */
 int _isalpha(int c)
-
+{
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-	
 }	
