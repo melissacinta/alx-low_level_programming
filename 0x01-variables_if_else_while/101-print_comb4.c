5013b95e36185446648a3ba13048c15ebb533fc3
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-17 17:20:59 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+	int j;
+
+	for (j = 0 ; j != 8 ; j++)
+	{
+		for (n = j + 1 ; n != 9 ; n++)
+		{
+			i = n + 1;
+			do {
+				putchar('0' + j);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (h != 7)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				i++;
+			} while (i != 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
