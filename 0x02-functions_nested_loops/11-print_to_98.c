88e10b5a52b9fdc2a068083050a8d1ed61f88a6c
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-18 17:45:14 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,11 +29,11 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d, " + n);
+				printf("%d, ", n);
 			}
 			else
 			{
-				printf("%d" + n);
+				printf("%d", n);
 			}
 			n--;
 		}
