3354e15d53b5c099366f22620470e138d2c52eea
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-18 18:44:30 -0700

    fixed bugs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,6 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-	
 		i = 0;
 		while (i <= n)
 		{
@@ -24,13 +23,13 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				}
 				else if (k < 10)
-                                {
-                                        _putchar(',');
-                                        _putchar(' ');
-                                        _putchar(' ');
+				{
+				       	_putchar(',');
+				       	_putchar(' ');
+				       	_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-                                }
+				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
@@ -39,12 +38,12 @@ void print_times_table(int n)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-				else
+				else if(k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-                                        _putchar(((k % 10) % 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				j++;
