2befef80be46fd83f74f303b480cd4cbdc66aaa1
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-21 15:08:04 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: the digit to be checked
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= '0' && c <= '9');
+}
