e4556916d6ff81f742bf5cb8b5c2d5e731599d19
Ngozi Aguluka <melissacinta@gmail.com>
2023-07-25 00:18:30 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints to the stdout
+ * @str : string to output
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
