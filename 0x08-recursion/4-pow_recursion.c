aa64bc67dfae856ab95072fa9261658398dac643
Ngozi Aguluka <melissacinta@gmail.com>
2023-08-08 14:37:52 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the factorial of a given number
+ * @x: number to to be multiplied
+ * @y: power
+ * Return: return the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(y - 1));
+}
