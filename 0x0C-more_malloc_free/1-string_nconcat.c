dcb036edc3d86f8bfc60435dd95a24edfff59422
Ngozi Aguluka <melissacinta@gmail.com>
2023-08-10 09:47:20 -0700

    fix bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1 && s1[len1])
 		len1++;
-	while (s2 && s2[len2] && n >= len2)
+	while (s2 && s2[len2])
 		len2++;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (n < len2)
+		s3 = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
 
@@ -30,7 +33,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 
-	while (i < (len1 + len2))
+	while (n < len2 && i < (len1 + n))
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	while (n >= len2 && i < (len1 + len2))
 	{
 		s3[i] = s2[j];
 		i++;
