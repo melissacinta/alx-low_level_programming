d33df59a24958bfb80bc068d5c1596d77bbed500
Ngozi Aguluka <melissacinta@gmail.com>
2023-08-17 05:15:57 -0700

    created a header file with struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure containing 3 elements of different types
+ * @name: name of the dog pointer to a char
+ * @age: age ofthe dog, pointer to a float
+ * @owner: owner of the dog, pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
