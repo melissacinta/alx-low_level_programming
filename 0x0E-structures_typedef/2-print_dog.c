f107e57c71f6d3b5b41bd7f49a03c88e69ecb8e9
Ngozi Aguluka <melissacinta@gmail.com>
2023-08-17 06:11:50 -0700

    added a function to print cont of the dog struct passed to it

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function to print members of dog struct on separate lines
+ * @d: struct to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
