d94c9d756816263bb0f574e3bb73d48514c854ac
Ngozi <melissacinta@gmail.com>
2023-08-19 13:59:16 +0000

    fixed null separator case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (!separator)
-		return;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
