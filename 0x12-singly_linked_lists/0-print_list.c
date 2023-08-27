610dbab01b5c14b7bb6fef9d10ec749794de10a6
Ngozi <melissacinta@gmail.com>
2023-08-27 15:47:56 +0000

    added function to print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: list to be printed;
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *temp;
+	int numEl = 0;
+
+	temp = (list_t *)malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (-1);
+	}
+
+	*temp = *h;
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		numEl++;
+	}
+
+	free(temp);
+	return (numEl);
+}
