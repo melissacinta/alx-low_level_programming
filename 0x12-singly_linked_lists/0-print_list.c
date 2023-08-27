6f0db4cad351db056cd130a8871a471a1aefd593
Ngozi <melissacinta@gmail.com>
2023-08-27 15:53:40 +0000

    added function to print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,24 +11,27 @@ size_t print_list(const list_t *h)
 	list_t *temp;
 	int numEl = 0;
 
-	temp = (list_t *)malloc(sizeof(list_t));
-	if (temp == NULL)
+	if (h)
 	{
+		temp = (list_t *)malloc(sizeof(list_t));
+		if (temp == NULL)
+		{
+			free(temp);
+			return (0);
+		}
+
+		*temp = *h;
+		while (temp)
+		{
+			if (temp->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%u] %s\n", temp->len, temp->str);
+			temp = temp->next;
+			numEl++;
+		}
+
 		free(temp);
-		return (0);
 	}
-
-	*temp = *h;
-	while (temp)
-	{
-		if (temp->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", temp->len, temp->str);
-		temp = temp->next;
-		numEl++;
-	}
-
-	free(temp);
 	return (numEl);
 }
