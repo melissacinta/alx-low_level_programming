46fb82cb65c46fcf173e6f245d62bfacf4285c7f
Ngozi <melissacinta@gmail.com>
2023-08-29 13:29:57 +0000

    fixed null check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (!(*head))
-		return;
-	while (*head)
+	if (head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		while (*head)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
