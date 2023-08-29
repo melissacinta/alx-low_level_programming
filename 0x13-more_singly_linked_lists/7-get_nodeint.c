56bb5097a5b8ccb536d4846751897c4671db8d86
Ngozi <melissacinta@gmail.com>
2023-08-29 14:14:34 +0000

    added function to get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_inde - function that returns the nth node of a linked list
+ * get_nodeint_at_index- function that returns the nth node of a linked list
  * @index: index of the node
  * @head: list to be tranversed
  * Return: NULL or the nth node
