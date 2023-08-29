1955e5a0c6376f9203fdbfe42c59a3e03da75b2b
Ngozi <melissacinta@gmail.com>
2023-08-29 13:15:05 +0000

    added function to free list and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  function that frees a list
+ * @head: start of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
