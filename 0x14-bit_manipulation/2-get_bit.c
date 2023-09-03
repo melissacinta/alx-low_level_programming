1713927cc27c144518993a6a53e0731de8207c36
Ngozi <melissacinta@gmail.com>
2023-09-03 13:53:15 +0000

    fixed error in get bit and added function to set bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,6 +3,7 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @index: is the index, starting from 0 of the bit you want to get
+ * @n: the integer to be worked with
  * Return: the value of the bit at index index or -1 if an error occured
  */
 
