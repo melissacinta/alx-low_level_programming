03670b69477978187bf95a0e463c6ed540758bfe
Ngozi Aguluka <melissacinta@gmail.com>
2023-09-06 01:28:43 +0000

    added function to append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t file, writef;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
 
