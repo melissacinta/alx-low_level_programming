0d3aefac3ad4c754ceb5c6a7cafd93fd4a7456e2
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-15 12:08:56 +0000

    fixed backward printing

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *h;
 
 	new = (void *)malloc(sizeof(dlistint_t));
 
@@ -17,9 +18,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	*h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
 	*head = new;
 
-	return (*head);
+	return (new);
 }
