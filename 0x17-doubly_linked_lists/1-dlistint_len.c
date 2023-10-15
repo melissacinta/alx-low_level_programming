747a63bcacbe188c3ee034f9700f4daa6ddd30c1
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-15 11:51:58 +0000

    fixed bug in code

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - a function that returns the number of elements in a 
+ * dlistint_len - a function that returns the number of elements in a
  * linked list
  * @h: pointer to the head node;
  */
