b74b6b429a3ac3648837de97a81558d136f82315
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-15 11:54:14 +0000

    fixed betty issues

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,8 @@
 /**
  * dlistint_len - a function that returns the number of elements in a
  * linked list
- * @h: pointer to the head node;
+ * @h: pointer to the head node
+ * Return:  returns the number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
