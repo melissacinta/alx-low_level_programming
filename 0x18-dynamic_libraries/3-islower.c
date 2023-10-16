aa8ccd1faffcc11e2c5e8d1b4d853c0074e7a1a3
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-16 14:43:59 +0000

    created dynamic library

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ *
+ * @c: The character to print
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
