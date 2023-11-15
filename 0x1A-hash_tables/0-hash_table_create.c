d2102a3c3f39f6bf7e427b7681085fa2e191b4a0
Ngozi Aguluka <melissacinta@gmail.com>
2023-11-15 17:03:27 +0000

    fixed betty errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of the table i.e total content
+ * Return: pointer to the created table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
