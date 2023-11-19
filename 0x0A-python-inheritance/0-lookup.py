ea46552ec1d7bd89204e3af42c338e0d8aad0de3
Ngozi Aguluka <melissacinta@gmail.com>
2023-11-19 13:51:51 +0000

    removed wrong push

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-"""Function to look up available attributes and methods of an object"""
-
-
-def lookup(obj):
-    """Return a list of attributes and method"""
-     return dir(obj)
